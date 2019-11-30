554013dacc21d93c77ca244a6949dfd2abe28c73
siddhant maurya <maurya2010boby@outlook.com>
2019-11-30 06:13:54 -0800

    Added entry renderer

---
+++ XamarinFormsRenderers/XamarinFormsRenderers.Android/Renderers/RoundedEntryRenderer.cs
@@ -0,0 +1,34 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamarinFormsRenderers.CustomRendererControls;
+using XamarinFormsRenderers.Droid.Renderers;
+
+[assembly:ExportRenderer(typeof(RoundedEntry),typeof(RoundedEntryRenderer))]
+namespace XamarinFormsRenderers.Droid.Renderers
+{
+    public class RoundedEntryRenderer : EntryRenderer
+    {
+        public RoundedEntryRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                var gradientDrawable = new GradientDrawable();
+                gradientDrawable.SetCornerRadius(60f);
+                gradientDrawable.SetStroke(5, Android.Graphics.Color.LightSeaGreen);
+                gradientDrawable.SetColor(Android.Graphics.Color.LightGray);
+
+                Control.SetBackground(gradientDrawable);
+            }
+        }
+    }
+}
---
+++ XamarinFormsRenderers/XamarinFormsRenderers/CustomRendererControls/RoundedEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinFormsRenderers.CustomRendererControls
+{
+    public class RoundedEntry : Entry
+    {
+    }
+}
